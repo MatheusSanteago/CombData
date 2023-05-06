1eb56e8bc746e92ab8d8ce39b5bd92ec95e16471
MatheusSanteago <matheussanteago@gmail.com>
2023-05-06 19:32:50 -0300

    Adicionando funções

--- main.py
+++ main.py
@@ -1,34 +1,59 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-df_full = pd.read_csv('./combustiveis-brasil.csv', delimiter=',')
+def transform(data):  # Transformação
+    data.rename(columns={'referencia': 'Referência',
+                         'ano': 'Ano',
+                         'mes': 'Mês',
+                         'gasolina_comum_preco_revenda_avg': 'Gasolina Comum',
+                         'gasolina_aditivada_preco_revenda_avg': 'Gasolina Aditivada',
+                         'etanol_hidratado_preco_revenda_avg': 'Etanol Hidratado',
+                         'oleo_diesel_preco_revenda_avg': 'Oléo Diesel',
+                         'oleo_diesel_s10_preco_revenda_avg': 'Oléo Diesel S10',
+                         'gas_cozinha_glp_preco_revenda_avg': 'GLP',
+                         'gas_natural_veicular_gnv_preco_revenda_avg': 'GNV',
+                         }, inplace=True)
+    newData = data[['Referência',
+                    'Ano',
+                    'Mês',
+                    'Gasolina Comum',
+                    'Gasolina Aditivada',
+                    'Etanol Hidratado',
+                    'Oléo Diesel',
+                    'Oléo Diesel S10',
+                    'GLP',
+                    'GNV']]
+    return newData
 
-meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio',
-         'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
-df_full.rename(columns={'referencia': 'Referência',
-                        'ano': 'Ano',
-                        'mes': 'Mês',
-                        'gasolina_comum_preco_revenda_avg': 'Gasolina Comum',
-                        'gasolina_aditivada_preco_revenda_avg': 'Gasolina Aditivada',
-                        'etanol_hidratado_preco_revenda_avg': 'Etanol Hidratado',
-                        'oleo_diesel_preco_revenda_avg': 'Oléo Diesel',
-                        'oleo_diesel_s10_preco_revenda_avg': 'Oléo Diesel S10',
-                        'gas_cozinha_glp_preco_revenda_avg': 'GLP',
-                        'gas_natural_veicular_gnv_preco_revenda_avg': 'GNV',
-                        }, inplace=True)
+def meanPearYear(data):  # Média por ano de cada combustível
+    mediaValorPorAno = data[['Ano', 'Gasolina Comum',
+                            'Gasolina Aditivada',
+                             'Etanol Hidratado',
+                             'Oléo Diesel',
+                             'Oléo Diesel S10',
+                             'GLP',
+                             'GNV']].groupby(['Ano'], as_index=False).mean()
+    plt.plot('Ano', 'Gasolina Comum',
+             data=mediaValorPorAno, label='Gasolina Comum')
+    plt.plot('Ano', 'Gasolina Aditivada',
+             data=mediaValorPorAno, label='Gasolina Aditivada')
+    plt.plot('Ano', 'Etanol Hidratado',
+             data=mediaValorPorAno, label='Etanol Hidratado')
+    plt.plot('Ano', 'Oléo Diesel', data=mediaValorPorAno, label='Oléo Diesel')
+    plt.plot('Ano', 'Oléo Diesel S10',
+             data=mediaValorPorAno, label='Oléo Diesel S10')
+    plt.plot('Ano', 'GNV', data=mediaValorPorAno, label='GNV')
+    plt.grid(True)
+    plt.xlabel('Anos')
+    plt.ylabel('Valor em R$')
+    plt.legend()
+    plt.ylim(0, 7)
+    plt.show()
 
-df_full.fillna(0, inplace=True)
 
-df = df_full[['Referência',
-              'Ano',
-              'Mês',
-              'Gasolina Comum',
-              'Gasolina Aditivada',
-              'Etanol Hidratado',
-              'Oléo Diesel',
-              'Oléo Diesel S10',
-              'GLP',
-              'GNV']]
-
-print(df.head(10))
+if __name__ == '__main__':  # Execucução
+    df_full = pd.read_csv('./combustiveis-brasil.csv', delimiter=',')
+    df = transform(df_full)
+    meanPearYear(df)
